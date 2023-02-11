f44cf46e6bb8e296663e8041ac5e04952460a680
Temidayo Kehinde <temidayo.kehinde.10@gmail.com>
2023-02-11 13:07:52 +0100

    Added two files

---
+++ index.js
@@ -0,0 +1,16 @@
+function add(num1, num2){
+    return num1 + num2
+}
+
+// add(10, 2);
+
+console.log(add(10, 2));
+
+let firstName = "Temidayo";
+let lastName = "Kehinde";
+let stuternTrack = "FrontEnd"
+let mySkills = "GitHub, HTML, CSS, and JavaScript";
+let favColor = "pink";
+let countryIndependence = 1960
+
+console.log("My name is " + firstName + " " + lastName + ". I am a " + stuternTrack + " Web Developer. My skills are " + mySkills + ". My favourite color is " + favColor + " and my country gained independence in " + countryIndependence + ".");
